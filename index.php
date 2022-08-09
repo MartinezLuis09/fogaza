32e2633f06f26fc21a477a9b201551459c1f8ef8
CesarZepeda23 <88913951+CesarZepeda23@users.noreply.github.com>
2022-08-09 11:03:41 -0500

    Avance

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    include("ecommerce/vistas/header.php");
+    include("ecommerce/vistas/navbar.php");
+    include("ecommerce/vistas/slider.php");
+    include("ecommerce/vistas/footer.php");
+?>
