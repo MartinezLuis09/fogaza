23bd896ff29b0cae63792ce8de2d656715576dd4
TaniaGisell <88916403+TaniaGisell@users.noreply.github.com>
2022-08-11 10:54:07 -0500

    cambio

--- ecommerce/modelo/mdl_buscador.php
+++ ecommerce/modelo/mdl_buscador.php
@@ -2,13 +2,19 @@
 include_once('_CRUD.php');
 
 Class Busqueda extends CRUD {
+  
+  function mostrarProductosBuscador() {
+    $query = "SELECT * FROM productos";
+    $sql = $this->_Select($query,null,"1");
+    return $sql;
+  } 
 
-      function prueba () {
+    /* function prueba () {
         $query = "SELECT DATE_FORMAT(NOW(),'%m-%d-%Y') ";
         $sql = $this->_Select($query,null,"1");
         foreach($sql as $row);
         return  $row[0];
-    }
+    } */
     
 }
 ?>
