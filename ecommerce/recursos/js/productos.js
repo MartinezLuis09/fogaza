e5cff22e08b10044d5a90e7f77ebf75a1b5b636f
CesarZepeda23 <88913951+CesarZepeda23@users.noreply.github.com>
2022-08-11 16:30:44 -0500

    Productos

--- ecommerce/recursos/js/productos.js
+++ ecommerce/recursos/js/productos.js
@@ -13,3 +13,20 @@ $(function() {
       },
     });
   });
+
+  $(function categorias() {
+    let datos = new FormData();
+    datos.append("opc", 2);
+    datos.append("id_categoria", n);
+    $.ajax({
+      type: "POST",
+      url: "../controlador/ctrl_Productos.php",
+      contentType: false,
+      data: datos,
+      processData: false,
+      cache: false,
+      success: function (respuesta) {
+      $("#contenedorProductosLista").html(respuesta); 
+      },
+    });
+  });
