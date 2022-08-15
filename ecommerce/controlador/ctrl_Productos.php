cb95211a482e6f4965b4b17cdde06cefd9e7f44f
TaniaGisell <88916403+TaniaGisell@users.noreply.github.com>
2022-08-15 15:48:10 -0500

    AVANCE

--- ecommerce/controlador/ctrl_Productos.php
+++ ecommerce/controlador/ctrl_Productos.php
@@ -12,25 +12,29 @@ switch($opc){
         foreach($sql as $row){
             $tarjetas = '
             <div class="gridProduct pointer col-6 col-lg-3 pt-4">
-            <div class="card">
-                <div class="rowList">
-                    <div class="imgList">
-                    <img src="../recursos/img/productos/principal/default.png?t='.time().'" class="card-img-top"
-                    alt="...">
-                    </div>
-                    <div class="bodyList">
-                        <div class="card-body pb-4" style="height: 200px;">
-                            <p class="card-title m-0 text-danger fw-bold"><strong>'.$row['titulo'].'</strong></p>
-                            <h4 class="card-text">$ '.$row['precio'].'</h4>
-                            <hr>
-                            <p class="card-text ellipsis-2 text-muted descripcion">
-                            '.$row['descripcion'].' 
-                            </p>
+                <div class="card" >
+                    <div class="rowList">
+                        <div class="imgList">
+                            <img src="../recursos/img/productos/principal/default.png?t=' . time() . '" class="card-img-top"
+                            alt="...">
+                        </div>
+                        <div class="bodyList">
+                            <div class="card-body pb-4" style="height: 65px;">
+                                <p class="card-title m-0 text-danger fw-bold"><strong>' . $row['titulo'] . '</strong></p>
+                            </div>
+                            <div class="card-body pb-4" style="height: 15px;">
+                                <h4 class="card-text">$'. $row['precio'] . '</h4>
+                            </div>
+                            <div class="card-body pb-4" style="height: 100px;">
+                                <hr>
+                                <p class="card-text ellipsis-2 text-muted descripcion">
+                                ' . $row['descripcion'] . ' 
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
             ';  
             echo $tarjetas;
            }  
@@ -52,25 +56,29 @@ switch($opc){
             foreach($sql as $row){
                 $tarjetasCat = '
                 <div class="gridProduct pointer col-6 col-lg-3 pt-4">
-                <div class="card">
-                    <div class="rowList">
-                        <div class="imgList">
-                        <img src="../recursos/img/productos/principal/default.png?t='.time().'" class="card-img-top"
-                        alt="...">
-                        </div>
-                        <div class="bodyList">
-                            <div class="card-body pb-4" style="height: 200px;">
-                                <p class="card-title m-0 text-danger fw-bold"><strong>'.$row['titulo'].'</strong></p>
-                                <h4 class="card-text">$ '.$row['precio'].'</h4>
-                                <hr>
-                                <p class="card-text ellipsis-2 text-muted descripcion">
-                                '.$row['descripcion'].' 
-                                </p>
+                    <div class="card" >
+                        <div class="rowList">
+                            <div class="imgList">
+                                <img src="../recursos/img/productos/principal/default.png?t=' . time() . '" class="card-img-top"
+                                alt="...">
+                            </div>
+                            <div class="bodyList">
+                                <div class="card-body pb-4" style="height: 65px;">
+                                    <p class="card-title m-0 text-danger fw-bold"><strong>' . $row['titulo'] . '</strong></p>
+                                </div>
+                                <div class="card-body pb-4" style="height: 15px;">
+                                    <h4 class="card-text">$'. $row['precio'] . '</h4>
+                                </div>
+                                <div class="card-body pb-4" style="height: 100px;">
+                                    <hr>
+                                    <p class="card-text ellipsis-2 text-muted descripcion">
+                                    ' . $row['descripcion'] . ' 
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
                 ';
                 echo $tarjetasCat;
                }  
